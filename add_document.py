ad09bee6392089faff5d2074eaa94e5af0b51a3c
Your Name <you@example.com>
2024-11-13 10:01:07 +0000

    8.3 replace deprecated imports

--- add_document.py
+++ add_document.py
@@ -4,10 +4,10 @@ import sys
 
 import pinecone
 from dotenv import load_dotenv
-from langchain.document_loaders import UnstructuredPDFLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
 
 load_dotenv()
 
